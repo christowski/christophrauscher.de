73db3473c42cdba6551f6e9e44c67ba3e247cf7c
Christoph Rauscher <mail@christowski.de>
2024-05-02 23:45:24 +0200

    add and move confusing stuff around

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,26 @@
 // Packages and plugins
 const fg = require("fast-glob");
 const rssPlugin = require('@11ty/eleventy-plugin-rss');
+const htmlmin = require("html-minifier");
 
 // Date filters
 const dateFilter = require('./src/filters/date-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
 
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addTransform("htmlmin", function (content) {
+		if ((this.page.outputPath || "").endsWith(".html")) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true,
+			});
+			return minified;
+		}
+		// If not an HTML output, return content as-is
+		return content;
+	});
 
   eleventyConfig.addWatchTarget("./src/sass");
 
